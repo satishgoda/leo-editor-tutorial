d35bf7ba1ded2b5bb8516a5be529f853db011177
Satish Goda <satishgoda@live.com>
2017-01-20 18:32:00 +0800

    Update mk_directive_nodes.py

--- sandbox/mk_directive_nodes.py
+++ sandbox/mk_directive_nodes.py
@@ -11,6 +11,7 @@ copied_p = p.copy()
 
 child_names = [child.h for child in p.children()]
 
+
 for line in sorted(os.listdir(cwd)):
     if line in child_names:
        g.es("Already exists: {0}".format(line), color='red')
@@ -22,14 +23,19 @@ for line in sorted(os.listdir(cwd)):
     child = copied_p.insertAsLastChild()
 
     if os.path.isfile(line):
-        node_pfx = '@@edit'
+        mapping = {
+            'py'  : '@@clean',
+            'png' : '@image',
+            'mp4' : '@movie',
+        }
         
-        if line.endswith(".py"):
-            node_pfx = '@@clean'
-        elif line.endswith('.png'):
-            node_pfx = '@image'
-        elif line.endswith('.mp4'):
-            node_pfx = '@movie'
+        elements = line.rpartition('.')
+
+        if not all(elements[:-1]):
+            node_pfx = '@@edit'
+        else:
+            node_pfx = mapping.get(elements[-1], '@@edit')
+
         child.h = '{0} {1}'.format(node_pfx, line)
     elif os.path.isdir(line):
         child.h = line
